12765449b7c3e1c3e4b5a1d6977f9d03e9773e76
yama662607 <yma662607@gmail.com>
2026-08-01 01:46:20 +0900

    chore: remove stale build artifacts from src

--- src/fs.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function writeFileAtomic(absolutePath: string, content: string): Promise<void>;
-export declare function readFileIfExists(absolutePath: string): Promise<string | null>;
